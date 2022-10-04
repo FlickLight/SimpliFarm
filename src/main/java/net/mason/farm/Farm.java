22aeff36c1c40d25332fd5cdbb4eb780375d0c53
5ofsp <5ofSpade1@gmail.com>
2022-10-03 23:54:45 -0400

    starting

---
+++ src/main/java/net/mason/farm/Farm.java
@@ -0,0 +1,15 @@
+package net.mason.farm;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Farm implements ModInitializer {
+	public static final String MOD_ID = "farm";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/mason/farm/FarmClient.java
@@ -0,0 +1,10 @@
+package net.mason.farm;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FarmClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
